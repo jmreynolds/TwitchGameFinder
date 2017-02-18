c09e02ce01bfc307a7d9059432054b52a35972b7
Joseph Reynolds <jreynolds@infocraft.net>
2017-02-17 18:38:16 -0600

    Initial Checkin

---
+++ TwitchGameFinder/TestHarness/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using TwitchService;
+
+namespace TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new TwitchGames();
+            int offset = 0;
+            var userInput = "";
+
+
+            while (userInput != "q")
+            {
+                var channels = foo.SearchGames(100, offset);
+                channels.Where(x => x.Channels < 30).ToList().ForEach(x =>
+                {
+                    Console.WriteLine($"Game: {x.Game.Name}    Channels: {x.Channels}    Viewers: {x.Viewers}");
+                });
+
+                Console.WriteLine("Press \"Q\" to quit, any other key to continue.");
+                var input = Console.ReadKey();
+                userInput = input.KeyChar.ToString().ToLower();
+                offset = offset + 100; 
+            }
+        }
+    }
+}
---
+++ TwitchGameFinder/TwitchService/TwitchGames.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TwitchLib;
+using TwitchLib.Models.API;
+
+namespace TwitchService
+{
+    public class TwitchGames
+    {
+        public List<GameByPopularityListing> SearchGames(int count = 10, int offset = 0)
+        {
+            TwitchApi.SetClientId("ym8haxs2lu162g80xjbqu62pxibebk");
+            var channelList = TwitchApi.GetGamesByPopularity(count, offset);
+            var gameByPopularityListings = channelList.Result.OrderByDescending(x=>x.Channels);
+            return gameByPopularityListings.ToList();
+        }
+    }
+}
